c1b4486e2c759316014ee083f9706506ca05672c
Viktor Horvat <vh51195@fer.hr>
2022-05-16 11:32:28 +0200

    update 1605
    UI update, code refactored

--- watchpstoryboard/ViewController.swift
+++ watchpstoryboard/ViewController.swift
@@ -7,14 +7,12 @@
 
 import UIKit
 import CoreBluetooth
-import CoreLocation
 import UserNotifications
 import Foundation
 
 
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
     
-    @IBOutlet var tableView: UITableView!
     var deviceNames = [String]()
     var centralManager: CBCentralManager!
     var myPeripheral: CBPeripheral!
@@ -22,15 +20,13 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     var allBluetoothPeripherals = [CBPeripheral]()
     
     let arrayOfServices: [CBUUID] = [CBUUID(string:"1414")]
-    
-    var currentConnectedName = "NULL"
+    let arrayOfProbabilities = ["No stimulus", "Wind", "Temperature", "Blue light", "Red light"]
     
     func scheduleNotification(){
         let content = UNMutableNotificationContent()
-        content.title = "Watchplant"
-        content.body = "Dostupan je novi senzor u blizini"
+        content.title = "Watchplant AI"
+        content.body = "New sensor is available"
         let date = Date().addingTimeInterval(1)
-        
         let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
         let uuidString = UUID().uuidString
@@ -38,14 +34,12 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
         notifCenter.add(request) { (error) in
                 }
-        print("NOTIFIKACIJA!")
     }
     
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         switch central.state {
         case .poweredOn:
-            print("BLE STATE ON")
             nameToShowLabel.text="Connecting"
             deviceNames.removeAll()
             central.scanForPeripherals(withServices: arrayOfServices, options: nil)
@@ -61,33 +55,24 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
             nameToShowLabel.text="Powered off"
         @unknown default:
             nameToShowLabel.text="Error"
-
         }
     }
     
-    
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         if let pname = peripheral.name {
-            print (pname)
             deviceNames.append(pname)
             centralManager.stopScan()
             centralManager.connect(peripheral, options: nil)
             allBluetoothPeripherals.append(peripheral)
             scheduleNotification()
-            print("DEBUG:_________________________")
-            print(advertisementData)
-            print("_________________________")
-            print(deviceNames)
         }
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        print("Connected")
         nameToShowLabel.text="CONNECTED"
         peripheral.delegate = self
         peripheral.discoverServices(nil)
     }
-
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         print("DEBUG: DEVICE SCANNED")
@@ -100,25 +85,23 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        print("DEBUG: Uređaj:\(peripheral) i servis na njemu: \(service)")
         peripheral.readValue(for: service.characteristics![0])
         peripheral.setNotifyValue(true, for: service.characteristics![0])
-        
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         let value = [UInt8] (characteristic.value!)
-        print (value)
+        rawDataLabel.text = "NEED EDIT" //NEED EDIT
+        interpretedDataLabel.text = arrayOfProbabilities[2] //NEED EDIT
+        
     }
 
-    @IBOutlet weak var pressureToShowLabel: UILabel!
-    @IBOutlet weak var humidityToShowLabel: UILabel!
+    @IBOutlet weak var interpretedDataLabel: UILabel!
+    @IBOutlet weak var rawDataLabel: UILabel!
     @IBOutlet weak var nameToShowLabel: UILabel!
-    @IBOutlet weak var temperatureToShowLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         centralManager = CBCentralManager(delegate: self, queue: nil)
-
     }
 }
